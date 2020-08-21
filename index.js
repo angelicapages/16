7266e992cb2c9d6d210ced363034d0eb19a4c730
angelicapages <angelicapages@gmail.com>
2020-08-21 20:39:13 -0300

    Primer y segundo ejercicio2

---
+++ index.js
@@ -0,0 +1,17 @@
+//EJERCICIO 1
+
+let nombre = "Angélica"
+
+alert (nombre)
+
+if (nombre.length = nombre.length) {
+    alert ("Mi nombre tiene" + " "+ (nombre.length) + " " + "letras")
+}
+
+//EJERCICIO 2
+
+let nombreYApellido = prompt ("Nombre y apellido")
+
+if (nombreYApellido.length = nombreYApellido.length) {
+    alert ("Tu nombre y apellido tiene" + " " + (nombreYApellido.length) + " " + "letras")
+}
